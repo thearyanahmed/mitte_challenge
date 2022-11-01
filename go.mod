3f0257c76f773f92447f9d024b8f23aa2cbc993e
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-01 20:50:49 +0100

    Dynamodb wip

--- go.mod
+++ go.mod
@@ -10,6 +10,19 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.17.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.23 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.1 // indirect
+	github.com/aws/smithy-go v1.13.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
@@ -18,6 +31,8 @@ require (
 
 require (
 	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go-v2/config v1.17.10
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.3
 	github.com/go-chi/chi v1.5.4
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/stretchr/testify v1.8.1
--- pkg/platform/aws.go
+++ pkg/platform/aws.go
@@ -1,11 +1,18 @@
 package platform
 
 import (
+	"context"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	awsv2 "github.com/aws/aws-sdk-go-v2/aws"
+	credentialsv2 "github.com/aws/aws-sdk-go-v2/credentials"
+	dynamodbv2 "github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func CreateAWSSession(accessKeyId, secretAccessKeyId, token, region string) (*session.Session, error) {
@@ -33,9 +40,31 @@ func createS3Credentials(accessKeyId, secretAccessKeyId, token, region string) (
 }
 
 func CreateDynamodbConnection(session *session.Session) *dynamodb.DynamoDB {
+	// dynamodbv2.New()
+	// @todo change dynamodb.New to v2
 	return dynamodb.New(session)
 }
 
 func Serve(handler interface{}) {
 	lambda.Start(handler)
+}
+func CreateLocalClient() *dynamodbv2.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(awsv2.EndpointResolverFunc(
+			func(service, region string) (awsv2.Endpoint, error) {
+				return awsv2.Endpoint{URL: "http://localhost:8000"}, nil
+			})),
+		config.WithCredentialsProvider(credentialsv2.StaticCredentialsProvider{
+			Value: awsv2.Credentials{
+				AccessKeyID: "dummy", SecretAccessKey: "dummy", SessionToken: "dummy",
+				Source: "Hard-coded credentials; values are irrelevant for local DynamoDB",
+			},
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return dynamodbv2.NewFromConfig(cfg)
 }
