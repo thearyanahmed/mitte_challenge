6949b8a7f35240c5d088359820ed022a5b82ca69
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-01 18:30:40 +0100

    Add test for unregistered route

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,39 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHandler(t *testing.T) {
+// func TestHandler(t *testing.T) {
+// 	tests := []struct {
+// 		request events.APIGatewayProxyRequest
+// 		expect  string
+// 		err     error
+// 	}{
+// 		{
+// 			// Test that the handler responds with the correct response
+// 			// when a valid name is provided in the HTTP body
+// 			request: events.APIGatewayProxyRequest{
+// 				Body: "Paul",
+// 				Path: "/user/create",
+// 			},
+// 			expect: "Hello Paul",
+// 			err:    nil,
+// 		},
+// 	}
 
-	tests := []struct {
-		request events.APIGatewayProxyRequest
-		expect  string
-		err     error
-	}{
-		{
-			// Test that the handler responds with the correct response
-			// when a valid name is provided in the HTTP body
-			request: events.APIGatewayProxyRequest{Body: "Paul"},
-			expect:  "Hello Paul",
-			err:     nil,
-		},
-		{
-			// Test that the handler responds ErrNameNotProvided
-			// when no name is provided in the HTTP body
-			request: events.APIGatewayProxyRequest{Body: ""},
-			expect:  "",
-			err:     nil,
-		},
+// 	for _, test := range tests {
+// 		response, err := handler(test.request)
+// 		assert.IsType(t, test.err, err)
+// 		assert.Equal(t, test.expect, response.Body)
+// 	}
+// }
+
+func TestUnregisteredRoute(t *testing.T) {
+	req := events.APIGatewayProxyRequest{
+		Path: "/random/endpoint/that/should/return/error",
 	}
 
-	for _, test := range tests {
-		response, err := handler(test.request)
-		assert.IsType(t, test.err, err)
-		assert.Equal(t, test.expect, response.Body)
-	}
+	resp, err := handler(req)
 
+	assert.IsType(t, err, nil)
+
+	stringBody, _ := json.Marshal(map[string]string{"message": "method not allowed."})
+	assert.Equal(t, string(stringBody), resp.Body)
 }
