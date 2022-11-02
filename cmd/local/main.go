d50ccb8c614b0c0fd821d216a69e6eb80f5687a7
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-11-02 13:15:33 +0100

    Add wait for table dynamodb

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/joho/godotenv"
 
 	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
@@ -32,15 +36,42 @@ func main() {
 		log.Fatalf("could not connect to database.%v\n", err)
 	}
 
+	fmt.Println(envValues)
+
 	aggregator := service.NewServiceAggregator(db)
 	r := routeHandler.SetupRouter(aggregator)
 
-	http.ListenAndServe(fmt.Sprintf("localhost:%s", getPort()), r)
+	err = waitForTable(context.Background(), db)
+	if err != nil {
+		log.Fatal("ERRRRRR", err)
+	}
+	addr := fmt.Sprintf("0.0.0.0:%s", getPort())
+
+	fmt.Println("Connected. Will be serving on : ", addr)
+
+	if err := http.ListenAndServe(addr, r); err != nil {
+		log.Fatalf("could not serve. %v\n", err)
+	}
+}
+
+func waitForTable(ctx context.Context, db *dynamodb.Client) error {
+	w := dynamodb.NewTableExistsWaiter(db)
+	err := w.Wait(ctx,
+		&dynamodb.DescribeTableInput{
+			TableName: aws.String("users"),
+		},
+		20*time.Second,
+		func(o *dynamodb.TableExistsWaiterOptions) {
+			o.MaxDelay = 5 * time.Second
+			o.MinDelay = 5 * time.Second
+		})
+
+	return err
 }
 
 func getPort() string {
 	if os.Getenv("APP_PORT") == "" {
-		return "8000"
+		return "8080"
 	}
 
 	return os.Getenv("APP_PORT")
